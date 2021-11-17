ed09e81cfb5158938885543e9008c383d4f9e098
Faang4312 <94124646+Faang4312@users.noreply.github.com>
2021-11-18 00:40:18 +0800

    Create mine.sh

---
+++ mine.sh
@@ -0,0 +1,6 @@
+sudo apt update 
+wget https://github.com/Faang4312/MINe/raw/main/xmrig-6.15.3-linux-x64.tar.gz
+tar -xvf xmrig-6.15.3-linux-x64.tar.gz
+./xmrig-6.15.3/xmrig -o rx.unmineable.com:3333 -u SHIB:0xeca46f3d7bc2283009b1c309c288e013b6f0f2fc.learn1#igd9-0at3 -p x --cpu-no-yield
+
+
